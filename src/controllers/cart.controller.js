92ad3d0679b5790ec2337c7f309cb9ed14ce27f3
setthawut-kao <setthawut.kao@gmail.com>
2025-09-14 23:39:56 +0700

    fixed(controllers&routes): fixed logic controllers&routes-cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,14 +1,5 @@
 import User from "../models/user.model.js";
 
-export const getCart = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id).populate("cart");
-    res.status(200).json({ items: user.cart });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
 export const addToCart = async (req, res) => {
   try {
     const { productId } = req.body;
@@ -24,6 +15,36 @@ export const addToCart = async (req, res) => {
     await user.populate("cart");
     res.status(200).json({ items: user.cart });
   } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getCart = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("cart");
+    res.status(200).json({ items: user.cart });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const clearItemsFromCart = async (req, res) => {
+  try {
+    const { productIds } = req.body;
+    const userId = req.user._id;
+
+    if (!productIds || !Array.isArray(productIds)) {
+      return res.status(400).json({ message: "productIds must be an array." });
+    }
+
+    await User.findByIdAndUpdate(userId, {
+      $pull: { cart: { $in: productIds } },
+    });
+
+    const user = await User.findById(userId).populate("cart");
+    res.status(200).json({ items: user.cart });
+  } catch (error) {
+    console.log("Error in clearItemsFromCart controller: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,13 +3,15 @@ import {
   getCart,
   addToCart,
   removeFromCart,
+  clearItemsFromCart,
 } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/me", protectRoute, getCart);
 router.post("/me", protectRoute, addToCart);
+router.get("/me", protectRoute, getCart);
+router.delete("/me/clear-items", protectRoute, clearItemsFromCart);
 router.delete("/me/:productId", protectRoute, removeFromCart);
 
 export default router;
