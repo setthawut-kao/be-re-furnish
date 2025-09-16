05a53d881768200c1f2f879f462122fdd6971021
setthawut-kao <setthawut.kao@gmail.com>
2025-09-16 10:03:19 +0700

    fixed(Deploy): can't used on iOs

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,15 +3,15 @@ import jwt from "jsonwebtoken";
 const generateTokenAndSetCookie = (userId, res) => {
   // 1. สร้าง JWT token โดยใช้ userId และ Secret Key ของเรา
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d", // Token มีอายุ 30 วัน
+    expiresIn: "1d", // Token มีอายุ 1 วัน
   });
 
   // 2. ตั้งค่า Token ให้เป็น httpOnly Cookie
   res.cookie("jwt", token, {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 วัน (หน่วยเป็น ms)
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
   });
 };
 
