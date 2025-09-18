e13abe02431f4cbdce09e52bcf48b6e13e767d52
setthawut-kao <setthawut.kao@gmail.com>
2025-09-18 15:00:53 +0700

    fixed(ratelimite): fixed ratelimite from 5 to 10 auth

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,14 +2,19 @@ import rateLimit from "express-rate-limit";
 
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 นาที
-  max: 100, // ปกติ: 100 req/IP
-  message: "Too many requests, please try again later.",
+  max: 200, // ปกติ: 100 req/IP
+  message: "Too many requests from this IP, please try again after 15 minutes.",
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 const authLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 นาที
-  max: 5, // route sensitive เช่น login/signup
-  message: "Too many login attempts, please try again later.",
+  windowMs: 15 * 60 * 1000, // 15min
+  max: 10,
+  message:
+    "Too many authentication attempts from this IP, please try again after 15 minutes.",
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 export { generalLimiter, authLimiter };
